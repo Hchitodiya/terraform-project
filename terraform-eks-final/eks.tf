78ebb8898d8acfecf567c59e754f2cfb517bff28
Ubuntu <ubuntu@ip-172-31-11-201.ap-south-1.compute.internal>
2025-06-01 05:36:19 +0000

    update the terraform project

--- terraform-eks-final/eks.tf
+++ terraform-eks-final/eks.tf
@@ -1,37 +1,30 @@
 module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "19.15.1"
 
-source            = "terraform-aws-modules/eks/aws"
-version           = "19.15.1"
+  cluster_name    = local.cluster_name
+  cluster_version = "1.31"
 
-cluster_name      = local.cluster_name
-cluster_version   = "1.31"
+  vpc_id     = var.vpc_id
+  subnet_ids = var.private_subnets
 
-subnet_ids  = module.vpc.private_subnets
-vpc_id      = module.vpc.vpc_id
+  enable_irsa = true
 
-enable_irsa = true
+  eks_managed_node_group_defaults = {
+    ami_type       = "AL2_x86_64"
+    instance_types = ["t3.medium"]
+    key_name       = var.key_name
+  }
 
-eks_managed_node_group_defaults = {
+  eks_managed_node_groups = {
+    private-ng = {
+      desired_size   = 2
+      max_size       = 3
+      min_size       = 2
 
-ami_type       = "AL2_x86_64"
-instance_types = ["t3.medium"]
-key_name       = var.key_name
-
+      instance_types = ["t3.medium"]
+      capacity_type  = "SPOT"
+    }
+  }
 }
 
-eks_managed_node_groups = {
-
-private-ng =  {
-
-desired_size   = 2
-max_size       = 3
-min_size       = 2
-
-instance_types     = ["t3.medium"]
-capacity_type      = "SPOT"
-
-}
-
-}
-
-}
--- terraform-eks-final/variable.tf
+++ terraform-eks-final/variable.tf
@@ -11,3 +11,21 @@ locals {
   cluster_name = "ubber-eks-cluster"
 }
 
+
+variable "vpc_id" {
+  description = "Existing VPC ID"
+  default     = "vpc-0bd462f56190fd83d"
+}
+
+variable "private_subnets" {
+  description = "List of existing private subnet IDs"
+  type        = list(string)
+  default     = ["subnet-07d1d8ccd944f52b9", "subnet-0009be1ce100c2462"]
+}
+
+variable "public_subnets" {
+  description = "List of existing public subnet IDs"
+  type        = list(string)
+  default     = ["subnet-0a833493af16fefd4", "subnet-02a4c4eea98945f0e"]
+}
+
